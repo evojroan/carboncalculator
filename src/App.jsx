4fca28d005a44592984bef9f173ccb614a2f12c2
Felizlin94 <felizlin94@gmail.com>
2023-04-23 14:42:05 +0800

    merge sidenav to main

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import styles from './App.module.scss';
 import SideNav from './components/SideNav/SideNav.jsx';
 import Main from './components/Main/Main.jsx';
 
-
 function App() {
   return (
     <div className={styles.AppContainer}>
