635d0327493f52f537011737c06c8a35c415ab35
evojroan <roan6903@gmail.com>
2023-04-23 12:03:40 +0800

    Main Page basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import SideNav from 'components/SideNav/SideNav.jsx';
+import Main from 'components/Main/Main.jsx';
 
 function App() {
   return (
